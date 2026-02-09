33491500cfcf1e822922a9c8f960299e35eacd21
Erifadina <nlegiaerifadina@gmail.com>
2026-02-09 11:47:28 +0700

    Add Swagger support and update README & changelog

--- Sipru.Backend/Program.cs
+++ Sipru.Backend/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Services
+// SERVICES
 
 // Controllers
 builder.Services.AddControllers();
@@ -15,16 +15,22 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     )
 );
 
-// OpenAPI / Swagger
-builder.Services.AddOpenApi();
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Middleware
+// MIDDLEWARE
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SIPRU Backend API v1");
+        c.RoutePrefix = "swagger"; // akses di /swagger
+    });
 }
 
 app.UseHttpsRedirection();
